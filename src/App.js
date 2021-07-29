3e8ce445901859f1e518a6864ca984793ccfe2b6
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-07-29 14:51:21 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet';
+//import Hello from './sayHello';
 
+
+
+//one component
 function App() {
+  const [isRed,setRed]= useState(false);
+  const [count, setCount] = useState(0);
+
+
+  //
+  const [user, setUser] = useState({
+    name: 'Matt',
+    age: 25,
+    posts: ['my first post']
+  });
+
+
+
+  //increment function
+  const increment = () => {
+    setCount(count+1);
+    //setRed(true);
+    setRed(!isRed);
+  };
+
+
+  /*Lesson 2: creating increment*/
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className={isRed ? 'red' : ""}>Change my color!</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
+    
   );
-}
+  
+} 
+
 
 export default App;
