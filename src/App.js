dfbd97594b6b056e19800d2d7a9ef3456ab87fb2
muhmp <defcoolz@gmail.com>
2021-09-11 18:19:45 +0900

    changes in name App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet';
+//import Hello from './sayHello';
 
+
+
+//one component
 function App() {
+  const [isRed,setRed]= useState(false);
+  const [count, setCount] = useState(0);
+
+  const [ users, setUser] = useState ([
+    {name: "Matt", message:" Hello there"},
+    {name: "Matt1", message:"I am Jo"},
+    {name: "Muh", message:"testing"},
+
+  ]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Simple tweet</h1>
+        {users.map(user =>(
+          <Tweet name={user.name} message={user.message} />
+        ))}
     </div>
-  );
-}
+  ); 
+} 
 
 export default App;
+
+    /*Lesson 2: creating increment*/
+
+  /*
+  const [user, setUser] = useState({
+    name: 'Matt',
+    age: 25,
+    posts: ['my first post']
+  });
+*/
+
+
+  /*
+  const increment = () => {
+    setCount(count+1);
+    //setRed(true);
+    setRed(!isRed);
+  };
+
+
+  return (
+    <div className="app">
+      <h1 className={isRed ? 'red' : ""}>Change my color!</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+    </div>
+  ); 
+} 
+
+*/
